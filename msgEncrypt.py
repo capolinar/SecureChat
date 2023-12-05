eb5256eb34d6b440c260a73e728484afd58beef9
Cody Apolinar <cnapolinar@cpp.edu>
2023-12-05 15:44:57 -0800

    Fixed encryption/decryption

--- msgEncrypt.py
+++ msgEncrypt.py
@@ -1,43 +1,85 @@
-import rsa
- 
-# generate public and private keys with 
-# rsa.newkeys method,this method accepts 
-# key length as its parameter
-# key length should be atleast 16
-publicKey, privateKey = rsa.newkeys(512)
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from base64 import b64encode, b64decode
 
-with open('PublicKey','wb') as file:
-	file.write(publicKey)
-     
+class AESCipher:
+    def __init__(self, key):
+        self.key = key
 
-#export private key for later use
-with open('PrivateKey', 'wb') as file:
-    file.write(privateKey)
+    def encrypt(self, message):
+        # Generate a random IV (Initialization Vector)
+        iv = b'\x00' * 16  # You should use a secure random generator for a real implementation
 
+        # Pad the message to make its length a multiple of the block size (128 bits for AES)
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(message.encode()) + padder.finalize()
 
+        # Create an AES cipher with CBC mode
+        cipher = Cipher(algorithms.AES(self.key), modes.CFB(iv), backend=default_backend())
 
+        # Encrypt the padded message
+        encryptor = cipher.encryptor()
+        cipher_text = encryptor.update(padded_data) + encryptor.finalize()
 
+        # Combine the IV and cipher text for transmission
+        result = iv + cipher_text
+        print("Encr")
+        return b64encode(result).decode()
 
- 
-# this is the string that we will be encrypting
+    def decrypt(self, encrypted_message):
+        # Decode the base64-encoded input
+        encrypted_data = b64decode(encrypted_message)
 
-message = input("Enter first message: ")
- 
-# rsa.encrypt method is used to encrypt 
-# string with public key string should be 
-# encode to byte string before encryption 
-# with encode method
-encMessage = rsa.encrypt(message.encode(), 
-                         publicKey)
- 
-print("original string: ", message)
-print("encrypted string: ", encMessage)
- 
-# the encrypted message can be decrypted 
-# with ras.decrypt method and private key
-# decrypt method returns encoded byte string,
-# use decode method to convert it to string
-# public key cannot be used for decryption
-decMessage = rsa.decrypt(encMessage, privateKey).decode()
- 
-print("decrypted string: ", decMessage)
+        # Extract the IV and cipher text
+        iv = encrypted_data[:16]
+        cipher_text = encrypted_data[16:]
+
+        # Create an AES cipher with CBC mode
+        cipher = Cipher(algorithms.AES(self.key), modes.CFB(iv), backend=default_backend())
+
+        # Decrypt the cipher text
+        decryptor = cipher.decryptor()
+        decrypted_padded_data = decryptor.update(cipher_text) + decryptor.finalize()
+
+        # Unpad the decrypted data
+        unpadder = padding.PKCS7(128).unpadder()
+        decrypted_data = unpadder.update(decrypted_padded_data) + unpadder.finalize()
+
+        return decrypted_data.decode()
+
+# Example usage
+key = b'Sixteen byte key'
+aes_cipher = AESCipher(key)
+
+# Encrypt a message
+message_to_encrypt = "This is a secret message!"
+encrypted_message = aes_cipher.encrypt(message_to_encrypt)
+print(f"Encrypted message: {encrypted_message}")
+
+# Decrypt the message
+decrypted_message = aes_cipher.decrypt(encrypted_message)
+print(f"Decrypted message: {decrypted_message}")
+
+def export_key_to_file(key, filename):
+    with open(filename, "wb") as key_file:
+        key_file.write(key)
+
+def import_key_from_file(filename):
+    with open(filename, "rb") as key_file:
+        return key_file.read()
+
+# Example usage
+keyx = b'Sixteen byte key'
+
+# Export the key to a file
+export_key_to_file(keyx, "AES_KEY")
+
+# Import the key from a file
+imported_key = import_key_from_file("AES_KEY")
+
+# Ensure the imported key matches the original key
+if key == imported_key:
+    print("Key import successful")
+else:
+    print("Key import failed")
--- server.py
+++ server.py
@@ -1,11 +1,58 @@
 import socket
 import threading
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from base64 import b64encode, b64decode
 
 HOST = '127.0.0.1'
 PORT = 65432
 
 CLIENT_LIST = []
 NICKNAMES = []
+#AES Encryption/Decryption
+def encrypt_message(key, message):
+    iv = b'\x00' * 16  # Use a secure random generator for a real implementation
+
+    if isinstance(message, str):
+        message_bytes = message.encode()
+    elif isinstance(message, bytes):
+        message_bytes = message
+    else:
+        raise TypeError("message must be a string or bytes-like object")
+
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(message_bytes) + padder.finalize()
+
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    cipher_text = encryptor.update(padded_data) + encryptor.finalize()
+
+    result = iv + cipher_text
+    return b64encode(result).decode()
+
+
+def decrypt_message(key, encrypted_message):
+    encrypted_data = b64decode(encrypted_message)
+    iv = encrypted_data[:16]
+    cipher_text = encrypted_data[16:]
+
+    cipher = Cipher(algorithms.AES(key), modes.CFB(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_padded_data = decryptor.update(cipher_text) + decryptor.finalize()
+
+    unpadder = padding.PKCS7(128).unpadder()
+    decrypted_data = unpadder.update(decrypted_padded_data) + unpadder.finalize()
+
+    return decrypted_data.decode()
+#AES Key import
+def import_key_from_file(AES_KEY):
+    with open(AES_KEY, "rb") as key_file:
+        return key_file.read()
+   
+AES_KEY = import_key_from_file("AES_KEY")
+
+
 
 
 def chat_server_single():
@@ -28,17 +75,20 @@ def chat_server():
     server_socket.bind((HOST, PORT))
     server_socket.listen(10)
 
-    def broadcast(message):
+    def broadcast(message):      
+        
         for client in CLIENT_LIST:
-            client.send(message)
+            encrypted_message = encrypt_message(AES_KEY, message)
+            client.send(encrypted_message.encode('utf-8'))
 
-    def handle(conn):
+    def handle(conn, nickname):
         while True:
             try:
-                message = conn.recv(1024)
-                if not message:
-                    break
-                broadcast(message)
+                encrypted_message = conn.recv(1024).decode('utf-8')
+                if not encrypted_message:
+                    break  
+                message = decrypt_message(AES_KEY, encrypted_message)
+                broadcast(f'{nickname}: {message}')
             except:
                 index = CLIENT_LIST.index(conn)
                 CLIENT_LIST.remove(conn)
@@ -59,8 +109,11 @@ def chat_server():
         conn.send(f'You have successfully connected to the server!\n'.encode())
         broadcast(f'{nickname} has joined the chat!'.encode())
 
-        thread = threading.Thread(target=handle, args=(conn,))
+        thread = threading.Thread(target=handle, args=(conn,nickname))
         thread.start()
+
+
+
 
 
 def main():
