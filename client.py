bed7fb05c4352fee526a60ab64b0f9b5b716538c
Cody Apolinar <cnapolinar@cpp.edu>
2023-12-05 16:49:13 -0800

    Added/Deleted comments for clarity

--- client.py
+++ client.py
@@ -12,7 +12,7 @@ PORT = 65432
 
 #AES Encryption
 def encrypt_message(key, message):
-    iv = b'\x00' * 16  # Use a secure random generator for a real implementation
+    iv = b'\x00' * 16  
 
     if isinstance(message, str):
         message_bytes = message.encode()
